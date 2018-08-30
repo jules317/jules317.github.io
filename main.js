be1631ca91645bf6dc4a34998a10de787060fcb2
jules317 <julia.h.spainhour@gmail.com>
2018-08-29 19:02:56 -0500

    Added titles and javascript

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $(".project-1").click(function(event){
+    event.preventDefault();
+    window.open("https://codepen.io/Jules316/pen/KBorQm", "_blank");
+  });
+  $(".project-2").click(function(event){
+    event.preventDefault();
+    window.open("https://codepen.io/Jules316/pen/ZrBjKY", "_blank");
+  });
+  $(".project-3").click(function(event){
+    event.preventDefault();
+    window.open("https://codepen.io/Jules316/pen/EQjrqd", "_blank");
+  });
+  $(".project-4").click(function(event){
+    event.preventDefault();
+    window.open("https://codepen.io/Jules316/pen/VXbWvM", "_blank");
+  });
+  $(".project-5").click(function(event){
+    event.preventDefault();
+    window.open("https://codepen.io/Jules316/pen/ZrRGPR", "_blank");
+  });
+  $(".project-6").click(function(event){
+    event.preventDefault();
+    window.open("https://codepen.io/Jules316/pen/rvNgEd", "_blank");
+  });
+
+})
